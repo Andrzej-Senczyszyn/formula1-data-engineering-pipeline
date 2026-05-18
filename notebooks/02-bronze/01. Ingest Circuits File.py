cb5f531583e265ece9570c122aa40f50d302b3f2
Andrzej-Senczyszyn <a.senczyszyn@op.pl>
2026-05-18 19:48:38 +0200

    Add bronze ingestion notebooks

---
+++ notebooks/02-bronze/01. Ingest Circuits File.py
@@ -0,0 +1,119 @@
+# Databricks notebook source
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Ingest circuits.csv file
+# MAGIC 1. Read the file using spark dataframe reader API
+# MAGIC 1. Add Metadata Columns 
+# MAGIC     - Source File
+# MAGIC     - Ingestion Timestamp
+# MAGIC 1. Write to bronze delta table    
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ![Incremental Data Processing](../07-images/formula1-incremental-data-processing.png "Incremental Data Processing")
+# MAGIC
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_batch_id", "")
+v_batch_id = dbutils.widgets.get("p_batch_id")
+
+
+# COMMAND ----------
+
+v_batch_id
+
+# COMMAND ----------
+
+# MAGIC %run ../00-common/01.environment-config
+
+# COMMAND ----------
+
+# MAGIC %run ../00-common/02.bronze-helpers
+
+# COMMAND ----------
+
+source_file = f"{landing_folder_path}/{v_batch_id}/circuits.csv"
+table_name = f"{catalog_name}.{bronze_schema}.circuits"
+
+
+# COMMAND ----------
+
+source_file
+
+# COMMAND ----------
+
+table_name
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 1 - Read the CSV file using the dataframe reader API
+# MAGIC
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
+
+circuits_schema = StructType([
+    StructField('circuitId',   StringType()),
+    StructField("url",         StringType()),
+    StructField("circuitName", StringType()),
+    StructField("lat",         DoubleType()),
+    StructField("long",        DoubleType()),
+    StructField("locality",    StringType()),
+    StructField("country",     StringType())
+])
+
+
+# COMMAND ----------
+
+circuits_df = (
+    spark.read
+         .format('csv')
+         .option('header', 'true')
+#         .option('inferSchema', 'true')
+         .option('mode', 'FAILFAST')
+         .schema(circuits_schema)
+         .load(source_file)
+)
+
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 2 - Add Metadata Columns
+# MAGIC - Source File
+# MAGIC - Ingestion Timestamp
+# MAGIC
+
+# COMMAND ----------
+
+circuits_final_df = add_ingestion_metadata(circuits_df)
+
+# COMMAND ----------
+
+display(circuits_final_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 3 - Write to bronze delta table
+
+# COMMAND ----------
+
+write_to_bronze (
+    input_df = circuits_final_df,
+    target_table = table_name,
+    batch_id = v_batch_id
+)
+
+
+# COMMAND ----------
+
